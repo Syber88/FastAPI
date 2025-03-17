d07e08a92c4c23f41d66b425e27d065c4ea15b82
Syber88 <siyamgz88@gmail.com>
2025-03-17 20:00:20 +0200

    added a way to update existing user with their user id

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from typing import List, Annotated
+from typing import List, Annotated, Optional
 import models 
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -13,6 +13,15 @@ class User(BaseModel):
     firstName: str
     lastName: str
     email: str
+    phone_number: str
+    
+class updateUser(BaseModel):
+    firstName: Optional[str] = None
+    lastName: Optional[str] = None
+    email: Optional[str] = None
+    phone_number: Optional[str] = None
+    
+    
     
 
 def getDb():
@@ -26,7 +35,7 @@ db_dependency = Annotated(Session, Depends(getDb))
 
 #creating users
 @app.post("/users/")
-async def create_User(user: User, db: db_dependency):
+async def create_User(user: User, db: Session = Depends(getDb)):
     current_user = db.query(models.Users).filter(models.Users.email == user.email).first()
     if current_user:
         raise HTTPException(status_code=400, detail="user already exitsts in the database")
@@ -39,7 +48,7 @@ async def create_User(user: User, db: db_dependency):
 
 #getting users with the id 
 @app.get("/users/{user_id}")
-async def get_user(user_id: int, db: db_dependency):
+async def get_user(user_id: int, db: Session = Depends(getDb)):
     result = db.query(models.Users).filter(models.Users.id == user_id).first()
     if not result:
         raise HTTPException(status_code=404, detail="user is not found")
@@ -47,11 +56,36 @@ async def get_user(user_id: int, db: db_dependency):
 
 #getting users with the email
 @app.get("/users/{user_email}")
-async def get_user_by_email(user_email: str, db: db_dependency):
+async def get_user_by_email(user_email: str, db: Session = Depends(getDb)):
     result = db.query(models.Users).filter(models.Users.email == user_email.email).first()
     if not result:
         raise HTTPException(status_code=404, detail="user is not found")
     return result
+
+#updating user 
+@app.post("/user/{user_id}")
+async def update_existing_user(user_id: int, user: updateUser, db: Session = Depends(getDb)):
+    db_user = db.query(models.Users).filter(models.Users.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="user is not found")
+    
+    if user.firstName:
+        db_user.firstName = user.firstName
+    if user.lastName:
+        db_user.name = user.lastName
+    if user.email:
+        db_user.name = user.email
+    if user.phone_number:
+        db_user.name = user.phone_number
+        
+    db.commit()
+    db.refresh(db_user)
+    
+    return {"user updated successfully, user ": db_user}
+    
+    
+    
+
 
     
 
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Users(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    firstName = Column(String, index=True)
     lastName = Column(String, index=True)
     email = Column(String, index=True, unique=True)
     phone_number = Column(String, index=True)
