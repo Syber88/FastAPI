511705ecfaca34d493ccdda92622a132d994da96
Syber88 <siyamgz88@gmail.com>
2025-03-17 13:09:53 +0200

    added user request definition and yielded databse4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List, annotated
+from typing import List, Annotated
 import models 
 from database import engine, SessionLocal
 from sqlalchemy.orm import session
@@ -8,6 +8,19 @@ from sqlalchemy.orm import session
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine) #will create all tables and columns in postgres
+
+class User(BaseModel):
+    firstName: str
+    lastName: str
+    email: str
+    
+
+def getDb():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
